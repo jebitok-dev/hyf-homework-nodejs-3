2c0c48b2be3bf63b294896f8e4281fec3a5d5f24
Jebitok <sharonkosgei4@gmail.com>
2020-11-06 04:55:02 +0300

    setup circle CI

---
+++ playground/notes.js
@@ -0,0 +1,5 @@
+// Build restful server using nodejs
+// building web apps -- split server & client
+// server - centralize & store data(http response)
+// client - interact with the data(http request)
+
